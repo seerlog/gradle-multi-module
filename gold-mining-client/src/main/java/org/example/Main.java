ac4b11052dd1ef3deb1bd472b28b679184ffa288
seerlog <selfandtogether@gmail.com>
2024-03-24 00:52:39 +0900

    docker compose 추가

--- gold-mining-client/src/main/java/org/example/Main.java
+++ gold-mining-client/src/main/java/org/example/Main.java
@@ -3,7 +3,8 @@ package org.example;
 import org.example.service.GoldMiningService;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(7000);
         GoldMiningService goldMiningService = new GoldMiningService();
         goldMiningService.goldMining();
     }
