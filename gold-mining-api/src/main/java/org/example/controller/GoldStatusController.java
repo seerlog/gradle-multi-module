b8fd3ea7e37b928b26fb1678fc2b49319812f6e3
seerlog <selfandtogether@gmail.com>
2024-06-06 22:11:46 +0900

    파드 배포하면서 api 설정 변경

--- gold-mining-api/src/main/java/org/example/controller/GoldStatusController.java
+++ gold-mining-api/src/main/java/org/example/controller/GoldStatusController.java
@@ -8,13 +8,35 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.InetAddress;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
-@RequiredArgsConstructor
-@RequestMapping(value = "/gold-status")
 public class GoldStatusController {
-    private final GoldStatusService goldStatusService;
+    private InetAddress ip;
+    private String uuid;
+    private GoldStatusService goldStatusService;
+
+    public GoldStatusController(GoldStatusService goldStatusService) {
+        try {
+            this.goldStatusService = goldStatusService;
+            this.ip = InetAddress.getLocalHost();
+            this.uuid = UUID.randomUUID().toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<String> getIp() {
+        return ResponseEntity.ok(this.ip.getHostAddress() + "\n");
+    }
+
+    @GetMapping("/gold/uuid")
+    public ResponseEntity<String> getUuid() {
+        return ResponseEntity.ok(this.uuid + "\n");
+    }
 
     @GetMapping("/{name}")
     public ResponseEntity<List<GoldStatusResponse>> getWorldGoldStatus(@PathVariable String name) {
